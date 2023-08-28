1698af0aabe894ed34b1f5aaa0a521851e6f184c
RadosavPanic <radosav.panic01@gmail.com>
2023-08-28 20:46:06 +0200

    Added more string methods

--- Basics/strings.py
+++ Basics/strings.py
@@ -27,11 +27,21 @@ print('{:&<10}'.format('testing'))  # testing&&&
 # MULTILINE
 statement = """This is a long multiline
 string where escape we characters\t like tab or newline\nworks"""
+
 print(statement)
 
 print(r'C:\\Program')  # C:\\Program, \r \R are raw string (no escape chars)
 print('C:\\Program')  # C:\Program
 
 # METHODS
+str1 = 'string example'
+
+print(str1[:6].capitalize() + str1[6:11].upper() + str1[11:].lower())  # String EXAMple
+print(str1.count("e", 0, 14))  # 2 (counts occurence of specified str)
+print(str1.center(16, '&'))  # &string example& (center string width, with both paddings)
+
+print(str1.startswith('stri'), str1.endswith('plea'))  # True False
+print(str1.find('ex', 3))  # 7 (returns index, -1 for not found)
+print(len(str1), str1.__len__())  # 14 (__len__ is internal command)
 
 
