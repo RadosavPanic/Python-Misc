38caf9dd9b8acdbb4439a8642cc19883bee34967
RadosavPanic <radosav.panic01@gmail.com>
2023-09-10 00:04:07 +0200

    added example for reduce with operator module

--- declaratives/reduce_fn.py
+++ declaratives/reduce_fn.py
@@ -1,9 +1,14 @@
 from functools import reduce
+import operator
 
 res = reduce(lambda x, y: x * y, [1, 2, 3, 4])
 print(res)  # 24
 
-seq1 = ["Testing shows the presence, not the absence of bugs"]
-reducedSeq = reduce(lambda x, y: x + y, seq1)
-print(reducedSeq)  # Testing shows the presence, not the absence of bugs
+seq1 = ["Testing", "shows", "the", "presence", "not", "the", "absence", "of", "bugs"]
+reducedSeq = reduce(lambda x, y: x + y + " ", seq1)
+print(reducedSeq)  # Testingshows the presence not the absence of bugs
+
+print(reduce(operator.add, seq1))  # Testingshowsthepresencenottheabsenceofbugs
+
+
 
